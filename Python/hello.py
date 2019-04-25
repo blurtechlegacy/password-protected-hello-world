fae9d830a75159cf3eb28b710c47377c0e37241a
Andrey Lomakin <lomus.mail@gmail.com>
2019-04-25 09:03:24 +0300

    Code.

---
+++ Python/hello.py
@@ -0,0 +1,5 @@
+password=input("Password:")
+try:
+    eval('brhi("print(\'Hello, world!\')".cvhwgihcb("".qhobcvhwg("qwertyuiopasdfghjklzxcvbnm", jhggnkvf)))'.translate("".maketrans("mnbvcxzlkjhgfdsapoiuytrewq", password)))
+except:
+    print("Incorrect password!")
