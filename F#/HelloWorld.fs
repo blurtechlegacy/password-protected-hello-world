7359c7a75e89e261c189e4f3ec11fcf8b8f0fc56
Антон Татарчук <taal@pkzdrav.ru>
2019-04-27 23:24:51 +0300

    Add f# solution

---
+++ F#/HelloWorld.fs
@@ -0,0 +1,8 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    let password = "1234"
+    printfn "Enter password"
+    if Console.ReadLine().Equals(password) then printfn "Hello world" else printfn "Incorrect password"
+    0
